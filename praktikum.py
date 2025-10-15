d33a08402f8be750c3728d29cbd6261dd706ae14
FahreziP <fahrezipurwadianitra@gmail.com>
2025-10-15 15:41:23 +0700

    third commit

--- praktikum.py
+++ praktikum.py
@@ -13,4 +13,8 @@ print(konversi_suhu(200, "C"))
 print(konversi_suhu(38, "F"))
 
 #2
+luas_lingkaran = lambda jari_jari: 3.14 * (jari_jari ** 2)
 
+n = float(input("Masukkan jari-jari lingkaran: "))
+
+print("Luas lingkaran dengan jari-jari" ,n, "adalah:", luas_lingkaran(n))
